98f3e124e1fbecd06da598e21dfdd3a834c7883b
Your Name <you@example.com>
2026-04-17 11:54:39 +0300

    chore: add build artifacts

--- pub/assets/main-Db_ictWx.js
+++ pub/assets/main-Db_ictWx.js
@@ -1,4 +1,4 @@
-import{r as n,j as e,c as b}from"./client-Bbs8k2yr.js";/**
+import{r as n,j as e,c as b}from"./client-DKqtRCEN.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
